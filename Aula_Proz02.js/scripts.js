4b4f262c5675f95c99a687c3f6fa7ec2c15e8c4c
Jefferson de Lima Gomes <86693914+Jeffersonlima578@users.noreply.github.com>
2023-11-20 11:28:16 -0300

    Update scripts.js

--- Aula_Proz02.js/scripts.js
+++ Aula_Proz02.js/scripts.js
@@ -1,26 +1,39 @@
 
-
-        addEventListener('load', function() {
-
-        var titulo = document.getElementById('titulo');
-
-        var listaNaoOrdenada = document.querySelector('ul');
-
-        var link = document.getElementById('link'); 
-
-        var listaOrdenada = document.getElementById('lista-ordenada');
+// Capturando os elementos h1 e a
+let titulo = document.getElementById('titulo')
+let link = document.querySelector('a');
 
 
+// Capturando o elemento <title>
+let tituloPagina = document.querySelector('title');
 
 
-
-    titulo.innerText = 'Título'; link.innerText = 'Proz Educação';
-
-    listaNaoOrdenada.innerHTML = 'Item 1 Item 2 Item 3';
-
-    listaOrdenada.innerHTML = 'Site 1 ' + ' Site 2 ' + ' Site 3';
+// Alterando o valor dentro da tag <title>
+tituloPagina.innerText = 'InnerText e HTML';
 
 
+// Adicionando conteúdo textual neles
+titulo.innerText = 'Temas estudados'
+link.innerText = 'Site da Proz'
 
 
-}); 
+// Capturando os elementos ul e ol
+let listaNaoOrdenada = document.querySelector('ul')
+let listaOrdenada = document.querySelector('ol')
+
+
+// Adicionando mais itens à lista não ordenada dinamicamente
+for (let i = 0; i < linguagens.length; i++) 
+{
+// Criando um novo elemento li para cada linguagem
+let novoItem = document.createElement('li');
+// Adicionando o texto da linguagem ao elemento li
+novoItem.innerText = linguagens[i];
+// Adicionando o novo item à lista
+listaNaoOrdenada.appendChild(novoItem);
+};
+
+// Adicionando três itens com links na lista ordenada
+listaOrdenada.innerHTML = `
+<li><a href="https://about.meta.com/">Meta</a></li>
+
